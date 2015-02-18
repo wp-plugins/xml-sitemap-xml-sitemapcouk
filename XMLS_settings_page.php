6f34e9f04ce9c48bc2e966857cc014546a575fbd
xmlsitemap <xmlsitemap@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-18 01:11:58 +0000

    code update
    git-svn-id: https://plugins.svn.wordpress.org/xml-sitemap-xml-sitemapcouk/trunk@1092972 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- XMLS_settings_page.php
+++ XMLS_settings_page.php
@@ -1,4 +1,16 @@
-<?php
+<div class="wrap">
+<h2><?php print XMLS_PUGIN_NAME ." ". XMLS_CURRENT_VERSION. "<sub>(Build ".XMLS_CURRENT_BUILD.")</sub>"; ?></h2>
+
+<form method="post" action="options.php">
+    <?php
+		settings_fields( 'XMLS-settings-group' );
+	?>
+    <table class="form-table">
+       
+        <tr valign="top">
+        <th scope="row"><a target="_blank" href="<?php echo site_url(); ?>/sitemap.xml">Click here</a> to view your WordPress XML Sitemap</th>
+            <th>OR</th>
+        <th scope="row"><?php
 $current_user = wp_get_current_user();
 $user = (array)$current_user->data;
 $user['url'] = "http://" . $_SERVER['SERVER_NAME'];
@@ -18,18 +30,7 @@ $url = "http://www.sitemaps.io/new/wp/install";
     $result = curl_exec($ch);
     //close connection
     curl_close($ch);
-?>
-<div class="wrap">
-<h2><?php print XMLS_PUGIN_NAME ." ". XMLS_CURRENT_VERSION. "<sub>(Build ".XMLS_CURRENT_BUILD.")</sub>"; ?></h2>
-
-<form method="post" action="options.php">
-    <?php
-		settings_fields( 'XMLS-settings-group' );
-	?>
-    <table class="form-table">
-       
-        <tr valign="top">
-        <th scope="row"><a target="_blank" href="<?php echo site_url(); ?>/sitemap.xml">Click here</a> to view your XML Sitemap</th>
+?> to view your Full XML Sitemap</th>
         <td></td>
         </tr>
     </table>
